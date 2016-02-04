c4527fcdee9991b09bfe087665c4460ca87adc5f
Suryadeep Pal <suryadeep10@gmail.com>
2016-02-04 23:28:44 +0530

    lib files included in main file

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var mongoose = require('mongoose');
 /* Config files */
 require('./config/configuration.js')(app, mongoose);
 
+/* Library Files for handling messages */
+require('./lib/incomingMessages.js')(app);
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on :' + process.env.PORT);
 });
