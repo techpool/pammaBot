a93603c2a1ba5b892355551a03bccfded88ae05e
Suryadeep Pal <suryadeep10@gmail.com>
2016-02-05 22:20:04 +0530

    api token saved as an express variable

--- config/configuration.js
+++ config/configuration.js
@@ -2,6 +2,8 @@ var bodyParser = require('body-parser');
 module.exports = function (app, mongoose) {
 
     app.set('teleBotLink', 'https://api.telegram.org/bot182492589:AAEs8klS3o14gMjXhXL4BI5e-7vdLUimOWU')
+    app.set('botToken', '182492589:AAEs8klS3o14gMjXhXL4BI5e-7vdLUimOWU');
+
 
     //Database connection string
     app.set('db', 'mongodb://Suryadeep:Surya-23121994@ds033285.mongolab.com:33285/mongocloud');
