1bec5de959b8c78b9b360ebeacb8001a16ff20dd
Suryadeep Pal <suryadeep10@gmail.com>
2016-02-06 09:32:38 +0530

    modified for public viewing

--- config/configuration.js
+++ config/configuration.js
@@ -1,12 +1,11 @@
 var bodyParser = require('body-parser');
 module.exports = function (app, mongoose) {
 
-    app.set('teleBotLink', 'https://api.telegram.org/bot182492589:AAEs8klS3o14gMjXhXL4BI5e-7vdLUimOWU')
-    app.set('botToken', '182492589:AAEs8klS3o14gMjXhXL4BI5e-7vdLUimOWU');
+    app.set('teleBotLink', 'https://api.telegram.org/botxxxxxxxxxxxxxx')
+    app.set('botToken', 'xxxxxxxxxxxxxxxxxx');
 
 
     //Database connection string
-    // app.set('db', 'mongodb://Suryadeep:Surya-23121994@ds033285.mongolab.com:33285/mongocloud');
     app.set('db', 'mongodb://localhost:27017/pammabot');
 
     //Using midleware to parse the body properly
