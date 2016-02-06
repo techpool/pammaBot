b75e150c9255386f60fe42c376e463a2acbf2ab4
Suryadeep Pal <suryadeep10@gmail.com>
2016-02-06 09:25:22 +0530

    new model included and lib files added

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ require('./config/configuration.js')(app, mongoose);
 
 /* Library Files for handling messages */
 require('./lib/incomingMessages.js')(app);
+Outgoing = require('./lib/outgoingMessages.js');
+StoreAndRetrieve = require('./lib/storeAndRetrieveMessages.js');
+
+/* Models for messages */
+Messages = require('./models/Messages.js')
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on :' + process.env.PORT);
